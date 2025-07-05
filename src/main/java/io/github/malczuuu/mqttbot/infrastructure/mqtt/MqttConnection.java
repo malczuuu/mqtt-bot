53b4516b0804dce477261883f467a3ece19d500f
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-07-05 13:03:54 +0200

    Switch to MemoryPersistence

--- src/main/java/io/github/malczuuu/mqttbot/infrastructure/mqtt/MqttConnection.java
+++ src/main/java/io/github/malczuuu/mqttbot/infrastructure/mqtt/MqttConnection.java
@@ -8,6 +8,7 @@ import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 public class MqttConnection {
 
@@ -42,7 +43,9 @@ public class MqttConnection {
   }
 
   private MqttClient connectMqttClient() throws MqttException {
-    MqttClient client = new MqttClient(broker.getServerUri(), UUID.randomUUID().toString());
+    MqttClient client =
+        new MqttClient(
+            broker.getServerUri(), UUID.randomUUID().toString(), new MemoryPersistence());
     MqttConnectOptions options = new MqttConnectOptions();
     options.setAutomaticReconnect(true);
     options.setCleanSession(true);
