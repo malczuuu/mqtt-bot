faf9cccc809a4170662d29a12ba486908a584f7b
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-07-23 20:16:54 +0200

    Initialize angular layout

--- mqtt-bot-fe/src/app/app.routes.ts
+++ mqtt-bot-fe/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'broker',
+    loadComponent: () => import('./pages/broker/broker').then(m => m.Broker)
+  },
+  {
+    path: 'settings',
+    loadComponent: () => import('./pages/settings/settings').then(m => m.Settings)
+  },
+  {
+    path: '',
+    redirectTo: 'broker',
+    pathMatch: 'full'
+  }
+];
--- mqtt-bot-fe/src/app/app.ts
+++ mqtt-bot-fe/src/app/app.ts
@@ -1,9 +1,13 @@
+
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterModule, RouterOutlet, MatToolbarModule, MatSidenavModule, MatListModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
