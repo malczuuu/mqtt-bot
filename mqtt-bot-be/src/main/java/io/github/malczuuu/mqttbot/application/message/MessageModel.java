9eeceb4a3fcad7b42315ba8e8471d3d76c2b8d86
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-07-25 21:58:37 +0200

    Use @JsonProperty

--- mqtt-bot-be/src/main/java/io/github/malczuuu/mqttbot/application/message/MessageModel.java
+++ mqtt-bot-be/src/main/java/io/github/malczuuu/mqttbot/application/message/MessageModel.java
@@ -1,4 +1,11 @@
 package io.github.malczuuu.mqttbot.application.message;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record MessageModel(
-    String id, String topic, String body, Long messageTime, Long publishTime, Long version) {}
+    @JsonProperty("id") String id,
+    @JsonProperty("topic") String topic,
+    @JsonProperty("body") String body,
+    @JsonProperty("message_time") Long messageTime,
+    @JsonProperty("publish_time") Long publishTime,
+    @JsonProperty("version") Long version) {}
