3abdd461dd2b44285db8cfb84486bc40a6474475
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-07-25 22:00:40 +0200

    Use @JsonProperty

--- mqtt-bot-be/src/main/java/io/github/malczuuu/mqttbot/application/message/MessageCreateModel.java
+++ mqtt-bot-be/src/main/java/io/github/malczuuu/mqttbot/application/message/MessageCreateModel.java
@@ -1,7 +1,9 @@
 package io.github.malczuuu.mqttbot.application.message;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record MessageCreateModel(
-    @NotNull @Size(min = 1, max = 1024) String topic, @NotNull String body) {}
+    @NotNull @Size(min = 1, max = 1024) @JsonProperty("topic") String topic,
+    @NotNull @JsonProperty("body") String body) {}
